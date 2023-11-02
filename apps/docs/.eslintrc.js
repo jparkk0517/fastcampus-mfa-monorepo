4644b8aaad66f7d31dd88701a626973b59e40b3d
unknown <redysj0517@naver.com>
2023-11-02 11:40:21 +0900

    feat: 1. 실습환경 및 초기 셋팅

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ["custom/react"],
+  env: {
+    node: true,
+    browser: true,
+  },
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off",
+  },
 };
--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,27 +1,7 @@
 import React from "react";
-import { Link } from "ui";
-import "./App.css";
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="header">
-          Docs
-          <div className="Turborepo">Turborepo Example</div>
-        </h1>
-        <div>
-          <Link className="App-link" href="https://turbo.build/repo">
-            Turborepo Docs
-          </Link>
-          <span> | </span>
-          <Link className="App-link" href="https://reactjs.org">
-            React Docs
-          </Link>
-        </div>
-      </header>
-    </div>
-  );
+  return <div className="App">docs app</div>;
 }
 
 export default App;
