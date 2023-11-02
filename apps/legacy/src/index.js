25509fc2e82a764b63c30d3e9fb90baeff287600
unknown <redysj0517@naver.com>
2023-11-02 15:41:24 +0900

    feat: 7-1. legacy 프로젝트의 생성 및 초기셋팅

---
+++ apps/legacy/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
